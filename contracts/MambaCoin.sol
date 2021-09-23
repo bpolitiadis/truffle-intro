d4891dd703c695a6c5c2d8a8eb12ca142a5a4a2f
bpolitiadis <b.politiadis@gmail.com>
2021-09-23 17:34:27 +0300

    cleaning1

--- contracts/MambaCoin.sol
+++ contracts/MambaCoin.sol
@@ -49,7 +49,6 @@ contract MambaCoin is Ownable, ERC20("MambaCoin","MAMBA") {
     function transfer(address _to, uint _amount) public override returns (bool) {
         _transfer(msg.sender, _to, _amount);
 
-      //record the payment...
         Payment memory new_payment = Payment({ 
             amount: _amount,
             recipient: _to,
@@ -72,26 +71,15 @@ contract MambaCoin is Ownable, ERC20("MambaCoin","MAMBA") {
        return records[_user];
    }
 
-    /**
-     * @dev shows all payments made by the user
-     * @return the array for Payment elements
-     */
     function ownRecords() external view returns (Payment[] memory) { 
         return records[msg.sender];
     }
 
-    /**
-     * @dev updates details of a payment
-     * @param _id the id of the payment
-     * @param _type the type of payment
-     * @param _comment a comment
-     */
     function updateDetails(uint _id, PaymentType _type, string calldata _comment) public {
         require(_id < currentPaymentId, "Non existing payment ID");
         require(uint(_type) < 5, "Invalid payment type");
         if (msg.sender==administrator){
             Log memory record = recordLog[_id];
-            //Payment storage payment = record[record.sender][] 
             records[record.sender][record.id].paymentType = _type;
             records[record.sender][record.id].comment = string(abi.encodePacked(_comment, " updated by ", Strings.toHexString(uint256(uint160(administrator)))));
         }
--- contracts/MambaNFT.sol
+++ contracts/MambaNFT.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/utils/Strings.sol";
 
 
 contract MambaNFT is Ownable, ERC721 {
